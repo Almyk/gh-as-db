fb257990834750d46f754d460019fa6749a2898e
almyk <almyk@tommyhansen.dev>
2026-01-15 08:00:55 +0900

    chore: bump version to 1.2.0

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { GitHubDB } from "./ui/github-db.js";
 export { Collection } from "./ui/collection.js";
 export { Transaction } from "./ui/transaction.js";
 export * from "./core/types.js";
-export const version = "1.0.1";
+export const version = "1.2.0";
